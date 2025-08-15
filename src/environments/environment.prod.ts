a7851ffe6a5a467eab0588b94bb7fcf6b3c0dc5e
sharanayya7 <mr.sharanayya@gmail.com>
2025-08-15 17:59:16 +0530

    config changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true
+};
+
+// This file is replaced during build by the `fileReplacements` array in `angular.json`.
+// When building for production, this file is used instead of `environment.ts`.
+// The list of file replacements can be found in `angular.json`.
